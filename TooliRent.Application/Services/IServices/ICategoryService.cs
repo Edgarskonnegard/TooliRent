3bf2e26b71a8207fefaa80f397f054511d7011d8
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-09-16 10:28:05 +0200

    added category interface

---
+++ TooliRent.Application/Services/IServices/ICategoryService.cs
@@ -0,0 +1,12 @@
+using TooliRent.Domain.Models;
+
+namespace TooliRent.Application.Interfaces;
+
+public interface ICategoryService
+{
+    Task<Category?> GetByIdAsync(int id, CancellationToken ct = default);
+    Task<IEnumerable<Category>> GetAllAsync(CancellationToken ct = default);
+    Task AddAsync(Category category, CancellationToken ct = default);
+    Task UpdateAsync(Category category, CancellationToken ct = default);
+    Task DeleteAsync(Category category, CancellationToken ct = default);
+}
