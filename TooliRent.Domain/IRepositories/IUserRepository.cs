0fa77c742326a67034ef740214ff1311b7a041ed
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-09-16 11:20:28 +0200

    update input type

--- TooliRent.Domain/IRepositories/IUserRepository.cs
+++ TooliRent.Domain/IRepositories/IUserRepository.cs
@@ -9,5 +9,5 @@ public interface IUserRepository
     Task<IEnumerable<User>> GetAllAsync(CancellationToken ct = default);
     Task AddAsync(User user, CancellationToken ct = default);
     Task UpdateAsync(User user, CancellationToken ct = default);
-    Task DeleteAsync(User user, CancellationToken ct = default);
+    Task DeleteAsync(int id, CancellationToken ct = default);
 }
