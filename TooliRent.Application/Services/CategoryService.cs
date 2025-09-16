1f9acc5e6d3330ff3df2f56e364cbfe17b2c909f
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-09-16 10:31:32 +0200

    added category service

---
+++ TooliRent.Application/Services/CategoryService.cs
@@ -0,0 +1,40 @@
+using TooliRent.Application.Interfaces;
+using TooliRent.Domain.Interfaces;
+using TooliRent.Domain.Models;
+
+namespace TooliRent.Application.Services;
+
+public class CategoryService : ICategoryService
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryService(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<Category?> GetByIdAsync(int id, CancellationToken ct = default)
+    {
+        return await _categoryRepository.GetByIdAsync(id, ct);
+    }
+
+    public async Task<IEnumerable<Category>> GetAllAsync(CancellationToken ct = default)
+    {
+        return await _categoryRepository.GetAllAsync(ct);
+    }
+
+    public async Task AddAsync(Category category, CancellationToken ct = default)
+    {
+        await _categoryRepository.AddAsync(category, ct);
+    }
+
+    public async Task UpdateAsync(Category category, CancellationToken ct = default)
+    {
+        await _categoryRepository.UpdateAsync(category, ct);
+    }
+
+    public async Task DeleteAsync(Category category, CancellationToken ct = default)
+    {
+        await _categoryRepository.DeleteAsync(category, ct);
+    }
+}
